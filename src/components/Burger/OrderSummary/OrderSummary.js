35cce5caa4bb2f33386c80c229351fad1049a3e3
ArsalanKm <arsalankarimzadeh1@gmail.com>
2021-01-26 09:42:32 +0330

    improvment added to Modal Component with ShoulComponent Life cycle

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,37 @@
 import React from "react";
 import Button from "../../Button/Button";
-const OrderSummary = (props) => {
-  console.log(props);
-  let { ingredients } = props;
-  const renderIngredients = Object.keys(ingredients).map((item, index) => {
+class OrderSummary extends React.Component {
+  componentWillUpdate(){
+    console.log('order sunnary update ')
+  }
+  render() {
+   
+    let { ingredients } = this.props;
+    const renderIngredients = Object.keys(ingredients).map((item, index) => {
+      return (
+        <li key={index}>
+          {item.toUpperCase()}:{ingredients[item]}
+        </li>
+      );
+    });
     return (
-      <li key={index}>
-        {item.toUpperCase()}:{ingredients[item]}
-      </li>
+      <>
+        <h3>Your Order</h3>
+        <p>Ingredients</p>
+        <ul>{renderIngredients}</ul>
+        <strong style={{ display: "block" }}>
+          price: {this.props.price.toFixed(2)}$
+        </strong>
+        <Button clicked={this.props.cancle} btnType="Danger">
+          {" "}
+          CANCEL
+        </Button>
+        <Button clicked={this.props.continue} btnType="Success">
+          CONTINUE
+        </Button>
+        <p>Continue to Checkout</p>
+      </>
     );
-  });
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>Ingredients</p>
-      <ul>{renderIngredients}</ul>
-      <strong style={{ display: "block" }}>price: {props.price.toFixed(2)}$</strong>
-      <Button clicked={props.cancle} btnType="Danger"> CANCEL</Button>
-      <Button clicked={props.continue} btnType="Success">CONTINUE</Button>
-      <p>Continue to Checkout</p>
-    </>
-  );
-};
-
+  }
+}
 export default OrderSummary;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,20 +2,31 @@ import React from "react";
 import styles from "./Modal.module.css";
 import Backdrop from "../BackDrop/BackDrop";
 
-const Modal = (props) => {
-  return (
-    <>
-      <Backdrop backDropHandler={props.backDropHandler} show={props.show} />
-      <div
-        className={styles.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.show ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </>
-  );
-};
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+  componentWillUpdate() {
+    console.log("modal will update");
+  }
+  render() {
+    return (
+      <>
+        <Backdrop
+          backDropHandler={this.props.backDropHandler}
+          show={this.props.show}
+        />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
 export default Modal;
